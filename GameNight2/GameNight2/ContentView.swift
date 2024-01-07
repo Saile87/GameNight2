b1e0548796ac410f4c6ab310509ed5e12325e318
Elias Breitenbach <elias.b@me.com>
2024-01-07 20:57:57 +0100

    Finish

--- GameNight2/GameNight2/ContentView.swift
+++ GameNight2/GameNight2/ContentView.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var names: [String] = []
+    
+    
     var body: some View {
+        VStack {
             Text("Hello, world!")
+            
+            Button(action: {
+                let player = "Elias"
+                names.append(player)
+            }) {
+                Text("B \(names.count)")
+            }
+        }
     }
 }
 
